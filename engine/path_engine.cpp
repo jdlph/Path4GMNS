f03ca7503d17088596c76357c8e2165167a86246
jdlph@MBP <jdlph@hotmail.com>
2023-01-22 18:26:05 -0500

    further improves shortest_path_n()
    - by making better use of stack memory.

--- engine/path_engine.cpp
+++ engine/path_engine.cpp
@@ -1,9 +1,14 @@
 /**
- * The following deque implementation is inspired by the efficient implementation
+ * The following deque implementations are inspired by the efficient implementation
  * from Dr. Hillel Bar-Gera
  *
  *     http://www.bgu.ac.il/~bargera/tntp/
  *     http://www.bgu.ac.il/~bargera/tntp/FW.zip
+ *
+ * shortest_path() and shortest_path_n() enhance the forgoing implementation by removing its duplicate
+ * checks on empty deque. shortest_path_n() further improves shortest_path() by making better use of
+ * stack memory (i.e., define deque_head and deque_tail within the for loop), which features the most
+ * efficient deque implementation of the modified label correcting (MLC) algorithm.
  *
  * Similar implementations can be also found in DYNASMART system designed by Dr. Hani Mahmassani and
  * the original code of DTALite by Dr. Xuesong Zhou.
@@ -59,6 +64,7 @@ void shortest_path(int o_node_no,
     // label correcting
     while (true)
     {
+        // filter out the TAZ based centroids
         if (cur_node >= first_thru_node || cur_node == o_node_no)
         {
             for (int k = first_link_from[cur_node]; k < last_link_from[cur_node]; ++k)
@@ -119,7 +125,7 @@ void shortest_path(int o_node_no,
         if (deque_head < 0)
             break;
 
-        // get the first node out of deque and use it as the current node.
+        // get the first node out of deque and use it as the current node
         cur_node = deque_head;
         deque_head = deque_next[cur_node];
         deque_next[cur_node] = was_in_deque;
@@ -147,92 +153,91 @@ void shortest_path_n(int o_node_no,
                      int departure_time)
 {
     // construct and initialize the following three on the first call only
-    static constexpr int invalid = -1, was_in_deque = -7;
+    static constexpr int nullnode = -1, was_in_deque = -7;
     static constexpr wchar_t all_mode[] = L"all";
 
-    // initialization
     for (int node_no = 0; node_no < node_size; ++node_no)
     {
-        // dueue_next is the implementation of scan eligible list for active nodes in label correcting
-        deque_next[node_no] = invalid;
-        // label cost, make it consistent with the python implementation
+        // dueue_next is the scan eligible list for active nodes in label correcting
+        deque_next[node_no] = nullnode;
         label_cost[node_no] = max_label_cost;
-        link_pred[node_no] = invalid;
-        node_pred[node_no] = invalid;
+        link_pred[node_no] = nullnode;
+        node_pred[node_no] = nullnode;
     }
 
-    // SEList initialization
-    int current_node = o_node_no;
-    int deque_head = invalid;
-    int deque_tail = invalid;
-    label_cost[current_node] = departure_time;
-    deque_next[current_node] = was_in_deque;
+    int cur_node = o_node_no;
+    label_cost[cur_node] = departure_time;
+    deque_next[cur_node] = was_in_deque;
 
     // label correcting
-    while (current_node != invalid && current_node != was_in_deque)
+    for(int deque_head = nullnode, deque_tail = nullnode;;)
     {
-        // used t filter out the TAZ based centroids
-        if (current_node <= last_thru_node || current_node == o_node_no)
+        // filter out the TAZ based centroids
+        if (cur_node <= last_thru_node || cur_node == o_node_no)
         {
-            for (int k = first_link_from[current_node]; k < last_link_from[current_node]; ++k)
+            for (int k = first_link_from[cur_node]; k < last_link_from[cur_node]; ++k)
             {
                 int link_seq_no = sorted_link_no_arr[k];
                 int new_node = to_node_no_arr[link_seq_no];
 
-                // if mode is "all", we are doing static shortest path calculation using distance
-                // and all links shall be considered; otherwise, mode shall be in link's allowed uses
-                //  or the allowed uses are for all modes (i.e., "all")
+                /**
+                 * if mode == 'a', we are doing static shortest path calculation using distance and
+                 * all links shall be considered; otherwise, mode shall be in link's allowed uses or
+                 * the allowed uses are for all modes (i.e., a)
+                 */
                 if (wcscmp(mode, all_mode) != 0
                     && !wcsstr(allowed_uses[link_seq_no], mode)
                     && !wcsstr(allowed_uses[link_seq_no], all_mode))
                     continue;
 
-                double new_cost = label_cost[current_node] + link_cost[link_seq_no];
+                double new_cost = label_cost[cur_node] + link_cost[link_seq_no];
                 if (label_cost[new_node] > new_cost)
                 {
                     label_cost[new_node] = new_cost;
                     link_pred[new_node] = link_seq_no;
                     node_pred[new_node] = from_node_no_arr[link_seq_no];
 
-                    // If the new node_indiex was in the queue before, add it as the first in the queue.
+                    /**
+                     * three cases
+                     *
+                     * case i:  new_node was in deque before, add it to the begin of deque
+                     * case ii: new_node is not in the queue, and wasn't there before, add it to the end of deque
+                     * case iii: new_node is in deque, do nothing
+                     */
                     if (deque_next[new_node] == was_in_deque)
                     {
                         deque_next[new_node] = deque_head;
                         deque_head = new_node;
 
-                        if (deque_tail == invalid)
+                        // deque is empty, initialize it.
+                        if (deque_tail == nullnode)
                             deque_tail = new_node;
                     }
-                    // If the new node_indiex is not in the queue, and wasn't there before,
-                    // add it at the end of the queue.
-                    else if (deque_next[new_node] == invalid && new_node != deque_tail)
+                    else if (deque_next[new_node] == nullnode && new_node != deque_tail)
                     {
-                        if (deque_tail != invalid)
+                        deque_tail = new_node;
+
+                        if (deque_tail == nullnode)
                         {
-                            // deque is not empty
-                            deque_next[deque_tail] = new_node;
-                            deque_tail = new_node;
+                            deque_head = new_node;
+                            deque_next[deque_tail] = nullnode;
                         }
                         else
-                        {
-                            // the queue is empty, initialize it.
-                            deque_head = deque_tail = new_node;
-                            deque_next[deque_tail] = invalid;
-                        }
+                            deque_next[deque_tail] = new_node;
                     }
-                    // If the new node_indiex is in the queue, just leave it there. (Do nothing)
                 }
             }
         }
 
-        // Get the first node_indiex out of the queue, and use it as the current node_indiex.
-        current_node = deque_head;
-        if (current_node == invalid || current_node == was_in_deque)
+        // deque is empty, terminate the process
+        if (deque_head < 0)
             break;
 
-        deque_head = deque_next[current_node];
-        deque_next[current_node] = was_in_deque;
-        if (deque_tail == current_node)
-            deque_tail = invalid;
+        // get the first node out of deque and use it as the current node
+        cur_node = deque_head;
+        deque_head = deque_next[cur_node];
+        deque_next[cur_node] = was_in_deque;
+        if (deque_tail == cur_node)
+            deque_tail = nullnode;
     }
 }
