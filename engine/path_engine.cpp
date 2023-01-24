a0f14137b2654a00494f5449292b3b3bfb33307f
jdlph@MBP <jdlph@hotmail.com>
2023-01-23 20:03:42 -0500

    continue refactoring the routing engines
    - replace bad namings.

--- engine/path_engine.cpp
+++ engine/path_engine.cpp
@@ -5,13 +5,13 @@
  *     http://www.bgu.ac.il/~bargera/tntp/
  *     http://www.bgu.ac.il/~bargera/tntp/FW.zip
  *
- * shortest_path() and shortest_path_n() enhance the forgoing implementation by removing its duplicate
+ * Similar implementations can be also found in DYNASMART system designed by Dr. Hani Mahmassani and
+ * DTALite by Dr. Xuesong Zhou.
+ *
+ * shortest_path() and shortest_path_n() enhance Bar-Gera's implementation by removing its duplicate
  * checks on empty deque. shortest_path_n() further improves shortest_path() by making better use of
  * stack memory (i.e., define cur_node, deque_head, and deque_tail within the for loop), which features
  * THE MOST efficient deque implementation of the modified label correcting (MLC) algorithm.
- *
- * Similar implementations can be also found in DYNASMART system designed by Dr. Hani Mahmassani and
- * the original code of DTALite by Dr. Xuesong Zhou.
  *
  * With constexpr, it is a C++ function (which requires C++11 or higher) rather than a pure C function.
  */
@@ -26,21 +26,21 @@ using std::wcsstr;
 using std::wcscmp;
 
 // keep it as legacy support for other packages using this old engine
-void shortest_path(int o_node_no,
+void shortest_path(int orig_node,
                    int node_size,
-                   const int* from_node_no_arr,
-                   const int* to_node_no_arr,
+                   const int* from_nodes,
+                   const int* to_nodes,
                    const int* first_link_from,
                    const int* last_link_from,
-                   const int* sorted_link_no_arr,
+                   const int* sorted_links,
                    const wchar_t** allowed_uses,
                    const double* link_cost,
-                   double* label_cost,
-                   int* node_pred,
-                   int* link_pred,
+                   double* label_costs,
+                   int* node_preds,
+                   int* link_preds,
                    int* deque_next,
                    const char mode,
-                   int departure_time,
+                   int depart_time,
                    int first_thru_node)
 {
     // construct and initialize the following two on the first call only
@@ -50,27 +50,27 @@ void shortest_path(int o_node_no,
     {
         // dueue_next is the scan eligible list for active nodes in label correcting
         deque_next[node_no] = nullnode;
-        label_cost[node_no] = INT_MAX;
-        link_pred[node_no] = nullnode;
-        node_pred[node_no] = nullnode;
+        label_costs[node_no] = INT_MAX;
+        link_preds[node_no] = nullnode;
+        node_preds[node_no] = nullnode;
     }
 
-    int cur_node = o_node_no;
+    int cur_node = orig_node;
     int deque_head = nullnode;
     int deque_tail = nullnode;
-    label_cost[cur_node] = departure_time;
+    label_costs[cur_node] = depart_time;
     deque_next[cur_node] = was_in_deque;
 
     // label correcting
     while (true)
     {
         // filter out the TAZ based centroids
-        if (cur_node >= first_thru_node || cur_node == o_node_no)
+        if (cur_node >= first_thru_node || cur_node == orig_node)
         {
             for (int k = first_link_from[cur_node]; k < last_link_from[cur_node]; ++k)
             {
-                int link_seq_no = sorted_link_no_arr[k];
-                int new_node = to_node_no_arr[link_seq_no];
+                int link = sorted_links[k];
+                int new_node = to_nodes[link];
 
                 /**
                  * if mode == 'a', we are doing static shortest path calculation using distance and
@@ -78,21 +78,21 @@ void shortest_path(int o_node_no,
                  * the allowed uses are for all modes (i.e., a)
                  */
                 if (mode != 'a'
-                    && !wcschr(allowed_uses[link_seq_no], mode)
-                    && !wcschr(allowed_uses[link_seq_no], 'a'))
+                    && !wcschr(allowed_uses[link], mode)
+                    && !wcschr(allowed_uses[link], 'a'))
                     continue;
 
-                double new_cost = label_cost[cur_node] + link_cost[link_seq_no];
-                if (label_cost[new_node] > new_cost)
+                double new_cost = label_costs[cur_node] + link_cost[link];
+                if (label_costs[new_node] > new_cost)
                 {
-                    label_cost[new_node] = new_cost;
-                    link_pred[new_node] = link_seq_no;
-                    node_pred[new_node] = from_node_no_arr[link_seq_no];
+                    label_costs[new_node] = new_cost;
+                    link_preds[new_node] = link;
+                    node_preds[new_node] = from_nodes[link];
 
                     /**
                      * three cases
                      *
-                     * case i:  new_node was in deque before, add it to the begin of deque
+                     * case i: new_node was in deque before, add it to the begin of deque
                      * case ii: new_node is not in the queue, and wasn't there before, add it to the end of deque
                      * case iii: new_node is in deque, do nothing
                      */
@@ -134,23 +134,23 @@ void shortest_path(int o_node_no,
     }
 }
 
-void shortest_path_n(int o_node_no,
+void shortest_path_n(int orig_node,
                      int node_size,
-                     const int* from_node_no_arr,
-                     const int* to_node_no_arr,
+                     const int* from_nodes,
+                     const int* to_nodes,
                      const int* first_link_from,
                      const int* last_link_from,
-                     const int* sorted_link_no_arr,
+                     const int* sorted_links,
                      const wchar_t** allowed_uses,
-                     const double* link_cost,
-                     double* label_cost,
-                     int* node_pred,
-                     int* link_pred,
+                     const double* link_costs,
+                     double* label_costs,
+                     int* node_preds,
+                     int* link_preds,
                      int* deque_next,
                      const wchar_t* mode,
                      int max_label_cost,
                      int last_thru_node,
-                     int departure_time)
+                     int depart_time)
 {
     // construct and initialize the following three on the first call only
     static constexpr int nullnode = -1, was_in_deque = -7;
@@ -160,24 +160,24 @@ void shortest_path_n(int o_node_no,
     {
         // dueue_next is the scan eligible list for active nodes in label correcting
         deque_next[node_no] = nullnode;
-        label_cost[node_no] = max_label_cost;
-        link_pred[node_no] = nullnode;
-        node_pred[node_no] = nullnode;
+        label_costs[node_no] = max_label_cost;
+        link_preds[node_no] = nullnode;
+        node_preds[node_no] = nullnode;
     }
 
-    label_cost[o_node_no] = departure_time;
-    deque_next[o_node_no] = was_in_deque;
+    label_costs[orig_node] = depart_time;
+    deque_next[orig_node] = was_in_deque;
 
     // label correcting
-    for(int cur_node = o_node_no, deque_head = nullnode, deque_tail = nullnode;;)
+    for(int cur_node = orig_node, deque_head = nullnode, deque_tail = nullnode;;)
     {
         // filter out the TAZ based centroids
-        if (cur_node <= last_thru_node || cur_node == o_node_no)
+        if (cur_node <= last_thru_node || cur_node == orig_node)
         {
             for (int k = first_link_from[cur_node]; k < last_link_from[cur_node]; ++k)
             {
-                int link_seq_no = sorted_link_no_arr[k];
-                int new_node = to_node_no_arr[link_seq_no];
+                int link = sorted_links[k];
+                int new_node = to_nodes[link];
 
                 /**
                  * if mode == 'a', we are doing static shortest path calculation using distance and
@@ -185,16 +185,16 @@ void shortest_path_n(int o_node_no,
                  * the allowed uses are for all modes (i.e., a)
                  */
                 if (wcscmp(mode, all_mode) != 0
-                    && !wcsstr(allowed_uses[link_seq_no], mode)
-                    && !wcsstr(allowed_uses[link_seq_no], all_mode))
+                    && !wcsstr(allowed_uses[link], mode)
+                    && !wcsstr(allowed_uses[link], all_mode))
                     continue;
 
-                double new_cost = label_cost[cur_node] + link_cost[link_seq_no];
-                if (label_cost[new_node] > new_cost)
+                double new_cost = label_costs[cur_node] + link_costs[link];
+                if (label_costs[new_node] > new_cost)
                 {
-                    label_cost[new_node] = new_cost;
-                    link_pred[new_node] = link_seq_no;
-                    node_pred[new_node] = from_node_no_arr[link_seq_no];
+                    label_costs[new_node] = new_cost;
+                    link_preds[new_node] = link;
+                    node_preds[new_node] = from_nodes[link];
 
                     /**
                      * three cases
