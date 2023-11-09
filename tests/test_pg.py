268ac49e201490bd0ee864cd6845dce1ef7cfc3b
jdlph <jdlph@hotmail.com>
2023-11-08 20:21:42 -0500

    introduce unit testing
    - modify demo.py to make comply with pytest.py;
    - rename demo.py to test_pg.py;
    - update .gitignore.

--- tests/test_pg.py
+++ tests/test_pg.py
@@ -1,13 +1,27 @@
-import path4gmns as pg
+from os import chdir
+
+from path4gmns.accessibility import evaluate_accessibility, evaluate_equity
+from path4gmns.colgen import perform_column_generation
+from path4gmns.dtaapi import perform_network_assignment_DTALite
+from path4gmns.simulation import perform_simple_simulation
+
+from path4gmns.utils import download_sample_data_sets, load_columns, load_demand,\
+                            output_agent_paths, output_agent_trajectory,\
+                            output_columns, output_link_performance, \
+                            output_synthesized_demand, output_zones, \
+                            read_network, read_zones
+
+from path4gmns.zonesyn import network_to_zones
+
 from time import time
 
 
 def test_download_sample_data_sets():
-    pg.download_sample_data_sets()
+    download_sample_data_sets()
 
 
 def test_find_shortest_path():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nshortest path (node id) from node 1 to node 2, '
           +network.find_shortest_path(1, 2))
@@ -27,7 +41,7 @@ def test_find_shortest_path_for_agents():
 
     print("DEPRECATED")
 
-    # network = pg.read_network(load_demand=True)
+    # network = read_network(load_demand=True)
 
     # st = time()
     # # find agent paths under a specific mode defined in settings.yaml,
@@ -59,31 +73,28 @@ def test_find_shortest_path_for_agents():
 
     # # output unique agent paths to a csv file
     # # if you do not want to include geometry info in the output file,
-    # # you can do pg.output_agent_paths(network, False)
-    # pg.output_agent_paths(network)
+    # # you can do output_agent_paths(network, False)
+    # output_agent_paths(network)
 
 
 def test_column_generation_py():
-    network = pg.read_network(load_demand=True)
+    network = read_network(load_demand=True, input_dir='fixtures')
 
     print('\nstart column generation\n')
     st = time()
 
     column_gen_num = 20
     column_update_num = 20
-    # pg.perform_network_assignment(assignment_mode=1, assignment_num,
-    #                               column_update_num, network)
-    # has been deprecated starting from v0.7.2, and will be removed later.
-    pg.perform_column_generation(column_gen_num, column_update_num, network)
+    perform_column_generation(column_gen_num, column_update_num, network)
 
     print(f'processing time of column generation: {time()-st:.2f} s'
           f' for {column_gen_num} iterations in column generation and '
           f'{column_update_num} iterations in column update')
 
     # if you do not want to include geometry info in the output file,
-    # use pg.output_columns(network, False)
-    pg.output_columns(network)
-    pg.output_link_performance(network)
+    # use output_columns(network, False)
+    output_columns(network)
+    output_link_performance(network)
 
 
 def test_column_generation_dtalite():
@@ -91,10 +102,12 @@ def test_column_generation_dtalite():
     print('start column generation using DTALite')
     st = time()
 
+    chdir('fixtures')
+
     mode = 1
     column_gen_num = 20
     column_update_num = 20
-    pg.perform_network_assignment_DTALite(mode, column_gen_num, column_update_num)
+    perform_network_assignment_DTALite(mode, column_gen_num, column_update_num)
 
     print(f'processing time of column generation: {time()-st:.2f} s'
           f' for {column_gen_num} iterations in column generation and '
@@ -102,14 +115,16 @@ def test_column_generation_dtalite():
 
     print('\npath finding results can be found in route_assignment.csv')
 
+    chdir('..')
+
 
 def test_loading_columns():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nstart loading columns\n')
     st = time()
 
-    pg.load_columns(network)
+    load_columns(network)
 
     print(f'processing time of loading columns: {time()-st:.2f} s')
 
@@ -118,29 +133,26 @@ def test_loading_columns():
 
     column_gen_num = 0
     column_update_num = 10
-    # pg.perform_network_assignment(assignment_mode=1, column_gen_num,
-    #                               column_update_num, network)
-    # has been deprecated starting from v0.7.2, and will be removed in later.
-    pg.perform_column_generation(column_gen_num, column_update_num, network)
+    perform_column_generation(column_gen_num, column_update_num, network)
 
     print(f'processing time of column generation: {time()-st:.2f} s'
           f' for {column_gen_num} iterations in column generation and '
           f'{column_update_num} iterations in column update')
 
-    pg.output_columns(network)
-    pg.output_link_performance(network)
+    output_columns(network)
+    output_link_performance(network)
 
 
 def test_accessibility():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nstart accessibility evaluation\n')
     st = time()
 
     # multimodal accessibility evaluation
-    pg.evaluate_accessibility(network)
+    evaluate_accessibility(network)
     # accessibility evaluation for a target mode
-    # pg.evaluate_accessibility(network, single_mode=True, mode='auto')
+    # evaluate_accessibility(network, single_mode=True, mode='auto')
 
     print('complete accessibility evaluation.\n')
     print(f'processing time of accessibility evaluation: {time()-st:.2f} s')
@@ -158,10 +170,10 @@ def test_accessibility():
     # time-dependent accessibility under the default mode auto
     # for demand period 0 (i.e., VDF_fftt1 in link.csv will be used in the
     # evaluation)
-    # pg.evaluate_accessibility(network, single_mode=True, time_dependent=True)
+    # evaluate_accessibility(network, single_mode=True, time_dependent=True)
 
     # it is equivalent to
-    # pg.evaluate_accessibility(network, single_mode=True,
+    # evaluate_accessibility(network, single_mode=True,
     #                           time_dependent=True, demand_period_id=0)
 
     # get accessible nodes and links starting from node 1 with a 5-minute
@@ -174,39 +186,39 @@ def test_accessibility():
 
 
 def test_equity():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nstart equity evaluation')
     st = time()
     # multimodal equity evaluation under default time budget (60 min)
-    pg.evaluate_equity(network)
+    evaluate_equity(network)
     # equity evaluation for a target mode with time budget as 30 min
-    # pg.evaluate_equity(network, single_mode=True, mode='auto', time_budget=30)
+    # evaluate_equity(network, single_mode=True, mode='auto', time_budget=30)
 
     print('complete equity evaluation.\n')
     print(f'processing time of equity evaluation: {time()-st:.2f} s')
 
 
 def test_zone_synthesis():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nstart zone synthesis')
     st = time()
-    pg.network_to_zones(network)
-    pg.output_zones(network)
-    pg.output_synthesized_demand(network)
+    network_to_zones(network)
+    output_zones(network)
+    output_synthesized_demand(network)
 
     print('complete zone and demand synthesis.\n')
     print(f'processing time of zone and demand synthesis: {time()-st:.2f} s')
 
 
 def test_loading_synthesized_zones_demand():
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
 
     print('\nstart loading synthesized zones and demand')
     st = time()
-    pg.read_zones(network)
-    pg.load_demand(network, filename='syn_demand.csv')
+    read_zones(network)
+    load_demand(network, filename='syn_demand.csv')
 
     print('complete loading synthesized zone and demand.\n')
     print(f'processing time of loading synthesized zone and demand: {time()-st:.2f} s')
@@ -215,74 +227,57 @@ def test_loading_synthesized_zones_demand():
     column_gen_num = 20
     column_update_num = 20
 
-    pg.perform_column_generation(column_gen_num, column_update_num, network)
+    perform_column_generation(column_gen_num, column_update_num, network)
 
-    pg.output_columns(network)
-    pg.output_link_performance(network)
+    output_columns(network)
+    output_link_performance(network)
 
 
 def test_simulation():
-    network = pg.read_network(load_demand=True)
+    network = read_network(load_demand=True, input_dir='fixtures')
 
-    column_gen_num = 20
-    column_update_num = 20
-    # pg.perform_column_generation(column_gen_num, column_update_num, network)
+    # column_gen_num = 20
+    # column_update_num = 20
+    # perform_column_generation(column_gen_num, column_update_num, network)
 
     # you can bypass the above perform_column_generation() and call
     # load_columns(network) if you have route_assignment.csv
-    pg.load_columns(network)
-    pg.perform_simple_simulation(network, 'uniform')
+    load_columns(network)
+    perform_simple_simulation(network, 'uniform')
     print('complete simple simulation.\n')
 
     print('writing agent trajectories')
-    pg.output_agent_trajectory(network)
+    output_agent_trajectory(network)
 
 
 def test_routing_engine():
     print('finding all-pairs shortest paths in the network\n')
-    network = pg.read_network()
+    network = read_network(input_dir='fixtures')
     network.benchmark_apsp()
 
 
-def demo_mode(mode):
-    print(f'the selected mode is {mode}\n')
-
-    if mode == 0:
-        # option 0: download the sample data set from GitHub
-        test_download_sample_data_sets()
-    elif mode == 1:
-        # option 1: find shortest path between O and D on Chicago network
-        test_find_shortest_path()
-    elif mode == 2:
-        # option 2: find shortest paths for all agents on Chicago network
-        test_find_shortest_path_for_agents()
-    elif mode == 3:
-        # option 3: perform column generation using Python engine
-        # on Chicago network
-        test_column_generation_py()
-    elif mode == 4:
-        # option 4: perform column generation using DTALite on Chicago network
-        test_column_generation_dtalite()
-    elif mode == 5:
-        # option 5: load columns generated from option 3 or 4
-        # on Chicago network
-        test_loading_columns()
-    elif mode == 6:
-        # option 6: evaluate multimodal accessibility on Chicago network
-        test_accessibility()
-    elif mode == 7:
-        # option 7: evaluate multimodal equity on Chicago network
-        test_equity()
-    elif mode == 8:
-        test_zone_synthesis()
-    elif mode == 9:
-        test_loading_synthesized_zones_demand()
-    elif mode == 10:
-        test_simulation()
-    else:
-        test_routing_engine()
-
-
-if __name__=="__main__":
-
-    demo_mode(4)
+# if __name__=="__main__":
+#     # test 0: download the sample data set from GitHub
+#     test_download_sample_data_sets()
+#     # test 1: find shortest path between O and D on Chicago network
+#     test_find_shortest_path()
+#     # test 2: find shortest paths for all agents on Chicago network
+#     test_find_shortest_path_for_agents()
+#     # test 3: perform column generation using Python engine on Chicago network
+#     test_column_generation_py()
+#     # test 4: perform column generation using DTALite on Chicago network
+#     test_column_generation_dtalite()
+#     # test 5: load columns generated from test 3 or 4 on Chicago network
+#     test_loading_columns()
+#     # test 6: evaluate multimodal accessibility on Chicago network
+#     test_accessibility()
+#     # test 7: evaluate multimodal equity on Chicago network
+#     test_equity()
+#     # test 8: synthesize zones and demand
+#     test_zone_synthesis()
+#     # test 9: load synthesized zones and demand
+#     test_loading_synthesized_zones_demand()
+#     # test 10: perform the simple DTA
+#     test_simulation()
+#     # test 11: test the C++ routing engine
+#     test_routing_engine()
