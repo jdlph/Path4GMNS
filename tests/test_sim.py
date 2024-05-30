c1aa425f4f977db5b467eabfa3ef11f6fe1f395e
jdlph@MBP <jdlph@hotmail.com>
2024-05-29 21:15:13 -0400

    fix the wrong work flow in test_sim.py
    - both demand file and the existing UE result were loaded if
      route_assignment.csv exits, which is wrong!

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -6,13 +6,15 @@ from path4gmns.utils import load_columns, read_network, output_agent_trajectory
 
 
 def test_simulation(sample_data_dir, tmp_output_dir):
-    network = read_network(input_dir=sample_data_dir)
+    network = None
 
     if isfile(tmp_output_dir + '/route_assignment.csv'):
+        network = read_network(load_demand=False, input_dir=sample_data_dir)
         # bypass perform_column_generation() and call load_columns(network)
         # when there is route_assignment.csv
         load_columns(network, input_dir=tmp_output_dir)
     else:
+        network = read_network(input_dir=sample_data_dir)
         column_gen_num = 20
         column_update_num = 20
         perform_column_generation(column_gen_num, column_update_num, network)
