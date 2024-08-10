5b2c267ff2d82ec05e8d74578028632061ecd765
jdlph@MBP <jdlph@hotmail.com>
2024-08-10 16:05:16 -0400

    automatically set up version number in setup.py
    - version number is automatically extracted from __init__.py;
    - enhance CMakeLists.txt with more customer targets.

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
-import setuptools
+import re
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
+def get_long_description():
+    with open('README.md', 'r') as fh:
+        return fh.read()
+
+
+def get_version():
+    init_file = 'path4gmns/__init__.py'
+    with open(init_file, 'r') as fh:
+        content = fh.read()
+
+    version = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if version:
+        return version.group(1)
+
+    raise RuntimeError('unable to find version info in __init__.py')
+
+
+setup(
     name="path4gmns",
-    version="0.9.9",
+    version=get_version(),
     author="Dr. Xuesong Zhou, Dr. Peiheng Li",
     author_email="xzhou74@asu.edu, jdlph@hotmail.com",
     description="An open-source, cross-platform, lightweight, and fast Python\
                 path engine for networks encoded in GMNS",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/jdlph/PATH4GMNS",
     packages=['path4gmns'],
