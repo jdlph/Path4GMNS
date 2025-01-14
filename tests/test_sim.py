0aee0a17041b25224c47fc3835a448f5aa63cb02
Haris <theoballisgr@gmail.com>
2025-01-14 14:21:25 +0200

    find_ue return total_gap, rel_gap and iteration number

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -2,14 +2,18 @@ from os.path import isfile
 
 from path4gmns.colgen import find_ue
 from path4gmns.simulation import perform_simple_simulation
-from path4gmns.io import load_columns, output_agent_trajectory, \
-                         read_demand, read_network
+from path4gmns.io import (
+    load_columns,
+    output_agent_trajectory,
+    read_demand,
+    read_network,
+)
 
 
 def test_simulation(sample_data_dir, tmp_output_dir):
     network = read_network(input_dir=sample_data_dir)
 
-    if isfile(tmp_output_dir + '/route_assignment.csv'):
+    if isfile(tmp_output_dir + "/route_assignment.csv"):
         # bypass perform_column_generation() and call load_columns(network)
         # when there is route_assignment.csv
         load_columns(network, input_dir=tmp_output_dir)
@@ -20,6 +24,6 @@ def test_simulation(sample_data_dir, tmp_output_dir):
         find_ue(network, column_gen_num, column_update_num)
 
     # simulation
-    perform_simple_simulation(network, 'uniform')
+    perform_simple_simulation(network, "uniform")
     # simulation output
     output_agent_trajectory(network, output_dir=tmp_output_dir)
--- tests/test_ue.py
+++ tests/test_ue.py
@@ -1,6 +1,11 @@
 from path4gmns.colgen import find_ue
-from path4gmns.io import load_columns, output_columns,\
-                         output_link_performance, read_demand, read_network
+from path4gmns.io import (
+    load_columns,
+    output_columns,
+    output_link_performance,
+    read_demand,
+    read_network,
+)
 
 
 def test_finding_ue(sample_data_dir, tmp_output_dir):
@@ -16,6 +21,22 @@ def test_finding_ue(sample_data_dir, tmp_output_dir):
     output_link_performance(network, output_dir=tmp_output_dir)
 
 
+def test_finding_ue_with_rel_gap_tolerance(sample_data_dir, tmp_output_dir):
+    network = read_network(input_dir=sample_data_dir)
+    read_demand(network, input_dir=sample_data_dir)
+
+    column_gen_num = 10
+    column_upd_num = 10
+    rel_gap_tolerance = 0.2
+
+    _, rel_gap, i = find_ue(
+        network, column_gen_num, column_upd_num, rel_gap_tolerance=rel_gap_tolerance
+    )
+
+    assert rel_gap <= rel_gap_tolerance
+    assert i <= column_upd_num
+
+
 def test_loading_columns(sample_data_dir, tmp_output_dir):
     network = read_network(input_dir=sample_data_dir)
     load_columns(network, input_dir=tmp_output_dir)
@@ -29,8 +50,7 @@ def test_loading_columns(sample_data_dir, tmp_output_dir):
 
 
 def test_mixed_invoking1(sample_data_dir, tmp_output_dir):
-    """ test resolution on issue #51 (https://github.com/jdlph/Path4GMNS/issues/51)
-    """
+    """test resolution on issue #51 (https://github.com/jdlph/Path4GMNS/issues/51)"""
     network = read_network(input_dir=sample_data_dir)
     read_demand(network, input_dir=sample_data_dir)
 
@@ -47,8 +67,7 @@ def test_mixed_invoking1(sample_data_dir, tmp_output_dir):
 
 
 def test_mixed_invoking2(sample_data_dir, tmp_output_dir):
-    """ test resolution on issue #51 (https://github.com/jdlph/Path4GMNS/issues/51)
-    """
+    """test resolution on issue #51 (https://github.com/jdlph/Path4GMNS/issues/51)"""
     network = read_network(input_dir=sample_data_dir)
     read_demand(network, input_dir=sample_data_dir)
 
