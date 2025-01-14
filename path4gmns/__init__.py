a35f847a3e30bffcc56a53733fa1155eda13d7a5
Haris <theoballisgr@gmail.com>
2025-01-14 14:24:35 +0200

    dump to v0.10.0

--- path4gmns/__init__.py
+++ path4gmns/__init__.py
@@ -8,8 +8,8 @@ from .utils import *
 from .zonesyn import *
 
 
-__version__ = '0.9.9.post1'
+__version__ = "0.10.0"
 
 
 # print out the current version
-print(f'path4gmns, version {__version__}\n')
+print(f"path4gmns, version {__version__}\n")
